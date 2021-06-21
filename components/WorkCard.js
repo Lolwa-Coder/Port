33eba7e8025b9fc7ff5541d1903071be4230aa0e
Prabhanshu Singh Sikarwar <64785251+Lolwa-Coder@users.noreply.github.com>
2021-06-21 20:16:18 +0530

    Update WorkCard.js

--- components/WorkCard.js
+++ components/WorkCard.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 function WorkCard({im,name}){
     return (
-        <div className="blur-3xl select-none flex-col   items-center cursor-pointer group  sm:h-screen/2 ">
-         <img src={im} className=" group-hover:blur-none duration-1000  h-screen/3"/>
+        <div className=" select-none flex-col   items-center cursor-pointer group  sm:h-screen/2 ">
+         <img src={im} className="   h-screen/3"/>
          <p className=" tracking-widest text-center text-5xl">{name}</p>
         </div>
     )
